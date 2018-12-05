b4398c0e7dcfcc496ccaf526fd68d03f4e3b97df
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2018-12-05 22:35:12 +0800

    Create campingListController.js

---
+++ Refactor Components and Communicate with Events/campingListController.js
@@ -0,0 +1,42 @@
+({
+    // Load items from Salesforce
+	doInit: function(component, event, helper) {
+
+    // Create the action
+    var action = component.get("c.getItems");
+
+    // Add callback behavior for when response is received
+    action.setCallback(this, function(response) {
+        var state = response.getState();
+        if (component.isValid() && state === "SUCCESS") {
+            component.set("v.items", response.getReturnValue());
+        }
+        else {
+            console.log("Failed with state: " + state);
+        }
+    });
+
+    // Send action off to be executed
+    $A.enqueueAction(action);
+},
+
+    
+    handleAddItem: function(component, event, helper) {
+    //   var newItem = event.getParam("item");
+    //helper.addItem(component, newItem);
+    var action = component.get("c.saveItem");
+    		action.setParams({"item": newItem});
+    		action.setCallback(this, function(response){
+        		var state = response.getState();
+        		if (component.isValid() && state === "SUCCESS") {
+            		// all good, nothing to do.
+            var items = component.get("v.items");
+            items.push(response.getReturnValue());
+            component.set("v.items", items);
+        		}
+    		});
+    		$A.enqueueAction(action);
+        		}
+    
+              
+})
