a9abd15c0def73ffc8ae7da723fb9024d99e497d
Miguel Garcia <30274681+shushuy@users.noreply.github.com>
2022-10-24 14:59:20 -0600

    Remove Extra `/>` to avoid SF Lint error

--- Refactor Components and Communicate with Events/campingListFormHelper.js
+++ Refactor Components and Communicate with Events/campingListFormHelper.js
@@ -7,7 +7,7 @@
                     'Name': '',
                     'Quantity__c': 0,
                     'Price__c': 0,
-                    'Packed__c': false } />);
+                    'Packed__c': false } );
 },
     
 
